2a20b4bb4eb3530dca374ca4489cc190b102ecbf
jcuella <74304981@continental.edu.pe>
2023-05-17 21:01:30 -0500

    MCD de tres números (rojo)

--- src/logica/OperacionesEnteros.py
+++ src/logica/OperacionesEnteros.py
@@ -1,3 +1,13 @@
 class OperacionesEnteros:
-    def MCD(self,  numero1, numero2):
+    def __init__(self, numeros):
+        self.__numeros = numeros
+
+    def MCD(self, numero1, numero2):
+        temporal = 0
+        while numero2 != 0:
+            temporal = numero2
+            numero2 = numero1 % numero2
+            numero1 = temporal
+        return numero1
+    def calcularMCD(self):
         return None
